013e285b45e83c3ce38b8190fa86c890bcc94fed
temilolakutelu <temilolakutelu@gmail.com>
2020-08-31 12:53:16 +0100

    deploy prep

--- src/main.js
+++ src/main.js
@@ -95,9 +95,9 @@ Vue.use(Loading, {
 Vue.component('v-select', vSelect)
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.component('ValidationObserver', ValidationObserver);
-var socket = io("http://marketsquareng.website:3300");
+// var socket = io("http://marketsquareng.website:3300");
 
-// var socket = io("localhost:3300");
+var socket = io("localhost:3300");
 Vue.prototype.$socket = socket;
 Vue.prototype.$request = Request;
 Vue.prototype.$swal = swal;
