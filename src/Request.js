c1a1f70f29c2185855521009151f0e532f7ce50a
Yondiame <kassimbalogun16@gmail.com>
2021-03-05 16:57:39 +0100

    moving to save

--- src/Request.js
+++ src/Request.js
@@ -10,8 +10,8 @@ import axios from 'axios';
 
 
 // const URL = "http://localhost:8000/api/";
- const URL = "https://marketsquareng.website/api/"
-// const URL = "https://admin.sundrymarkets.com/api/"
+//  const URL = "https://marketsquareng.website/api/"
+const URL = "https://admin.sundrymarkets.com/api/"
 
 export default {
     name: "Request",
