ef2804f6c9d0b2f2827c18ef8170314da11dbbc2
taiwoadetayo <82810641+taiwoadetayo@users.noreply.github.com>
2021-06-16 15:40:31 +0100

    seach filter update

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -31,7 +31,7 @@
                     </div>
                     <div v-else>
                         <span v-if="isNotFound(result)" class="description">
-                            {{ result + ' for ' }} <b class="searchQuery"> {{ searchQuery }} </b>
+                            {{ result + ' for ' }} <b class="searchQuery"> {{ searchQuery }} in {{category_name}} </b>
                         </span>
                         <span v-else class="description">
                             <b class="searchQuery"> {{ searchQuery }} </b> {{ ' in ' + result }}
