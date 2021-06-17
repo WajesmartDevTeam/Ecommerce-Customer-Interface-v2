33906c767f78c47f3634eca6968d8618c2b38d59
RuthRudi <77901890+RuthRudi@users.noreply.github.com>
2021-06-17 10:23:27 +0100

    Delivery Fee Variation

--- src/Request.js
+++ src/Request.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 
 
 //const URL = "http://127.0.0.1:8000/api/";
-// const URL = "http://localhost:8000/api/";
+ //const URL = "http://localhost:8000/api/";
 const URL = "https://marketsquareng.website/api/" //
 //const URL = "https://admin.sundrymarkets.com/api/" //
 
